e2406a31a7aa2b314f9b1ee1651b71609c48aadf
Maria-92 <55132033+Maria-92@users.noreply.github.com>
2021-08-23 15:57:38 +0400

    Update Randomisation.swift

--- Randomisation.swift
+++ Randomisation.swift
@@ -1,4 +1,7 @@
   
+
+   // Q : Pick 6 Random Alphabet From the 26 Letters to Create 6 random Character password .. 
+
   let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
     //The number of letters in alphabet equals 26
